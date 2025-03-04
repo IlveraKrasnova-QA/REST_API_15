4608ee877ca01abef5f3a4ab9c86263563e4ed6d
krasnova.ilvera <ilvera.krasnova@yandex.ru>
2025-03-04 22:29:09 +0300

    added TestBase

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -2,8 +2,8 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
-    @BeforeAll
-    public static void setUp() {
+        @BeforeAll
+        public static void setUp() {
         RestAssured.baseURI = "https://reqres.in";
         RestAssured.basePath = "/api";
     }
